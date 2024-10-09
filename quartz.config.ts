e6178bad37b0c176a6beef900c51f199d65ba7ab
Generic-person4077 <156739813+Generic-person4077@users.noreply.github.com>
2024-10-09 12:05:35 -0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -82,7 +82,10 @@ const config: QuartzConfig = {
       sort: (f1, f2) => {
     const f1Title = f1.frontmatter?.title.toLowerCase() ?? ""
     const f2Title = f2.frontmatter?.title.toLowerCase() ?? ""
-    return f1Title.localeCompare(f2Title)
+    return f1Title.localeCompare(f2Title, undefined, {
+        numeric: true,
+        sensitivity: "base",
+      })
   }
       }),
       Plugin.TagPage(),
